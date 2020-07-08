ef759e7efaa40f600a54fc61c69282da7e5f0305
Evgeny-1975 <kostykov_e@mail.ru>
2020-07-08 13:54:32 +0600

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,22 @@
 $(function () {
    
-   
-   var mixer = mixitup('.products__inner-box');
+   $(".rate-star").rateYo({
+      rating: 5,
+      readOnly: true,
+      starWidth: "12px"
+   });
+
+   $('.product-slider__inner').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: false,
+   //autoplay: true,
+   //autoplaySpeed: 2000
+   });
+
+ var mixer = mixitup('.products__inner-box');
 
 
 });
+
